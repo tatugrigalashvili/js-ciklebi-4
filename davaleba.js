ae1c0250e85cdedc0150ca289aa0d8230d3d53d6
tatugrigalashvili <146704405+tatugrigalashvili@users.noreply.github.com>
2024-05-25 20:59:58 +0400

    Add files via upload

---
+++ davaleba.js
@@ -0,0 +1,13 @@
+
+var masivi = [1, 2, -9, -56, 34, 77, -90, -11, -1, -2, -5, -9];
+
+var i = 0;
+
+while (i < masivi.length) {
+  
+  if (masivi[i] < 0) {
+    document.write(masivi[i] + "<br>");
+  }
+  
+  i++;
+}
